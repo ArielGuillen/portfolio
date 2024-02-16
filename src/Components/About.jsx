b324f25992544ec4058b47766a35d2082cd34e3a
Ariel Guillen <ariel.guillen.dev@gmail.com>
2024-02-16 23:05:25 +0000

    feat: :rocket: update portfolio data

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,31 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/background-white.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "white abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a backend developer with solid experience in creating and optimizing project architectures.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "APIs",
+  "Data Modeling",
+  "Authentication",
+  "Validation",
+  "Database management",
+  "Performance optimization",
+  "Code review",
+  "Continuous integration",
+  "Continuous deployment",
+  "Agile methodologies",
 ];
 
 /**
@@ -45,7 +49,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My focus is on effective problem solving and code optimization to ensure exceptional performance. My ability to learn quickly allows me to adapt to new challenges and technologies with ease. With an ongoing commitment to technical excellence, I am ready to contribute my experience to ambitious projects and continue growing in the exciting world of backend development.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desktop.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desktop with keyboard and monitor";
 
 /**
  * Project list
@@ -31,28 +31,20 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "NestJS GraphQL API 🚀",
+    description: "This repository shows how to implement a GRAPHQL API using Nestjs.",
+    url: "https://github.com/ArielGuillen/nest-graphql-example",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Apollo Rust Plugins 🚀🦀",
+    description: "This repository contains the source code for examples Apollo Rust plugins.",
+    url: "https://github.com/DanielAcostaRoa/apollorouter-rust",
   },
   {
-    title: "My Resume Site",
+    title: "AWS S3 Unzipper 📦",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a js script that unzips a file from an S3 bucket and uploads it back to the same bucket.",
+    url: "https://github.com/ArielGuillen/s3-unzip",
   },
 ];
 
